716d71158cc69bc294cfd379e07b0200c31c82e9
Kisspeace <101427274+Kisspeace@users.noreply.github.com>
2023-06-28 20:19:13 -0700

    Update download_assets.bat.

--- test/download_assets.bat
+++ test/download_assets.bat
@@ -1,7 +1,10 @@
 @ECHO OFF
 SET download_dir=temp\
+SET useragent="Mozilla/5.0 (Windows NT 10.0; rv:114.0) Gecko/20100101 Firefox/114.0"
+SET timeouts=3
 @MKDIR %download_dir%
 
+call :add e621 "https://e621.net/posts?tags=equine_penis+female" "https://e621.net/posts/4131064?q=equine_penis+female" ""
 call :add kenzatouk "https://kenzato.uk/booru/explore/recent" "https://kenzato.uk/booru/image/GdhwKa" "AGREE_CONSENT=1; CHV_COOKIE_LAW_DISPLAY=0;"
 call :add dbbepismoe "https://db.bepis.moe/koikatsu?orderby=popularity" "https://db.bepis.moe/koikatsu/view/303101" ""
 call :add bleachbooru "https://bleachbooru.org/posts?page=1" "https://bleachbooru.org/posts/68270" ""
@@ -22,7 +25,7 @@ EXIT /B 0
 	SET cookie=%4
     echo "-- DOWNLOADING %name%"
 
-    curl -# -fo "%download_dir%%name%_list.html" %url_list% --cookie %cookie%
-	curl -# -fo "%download_dir%%name%_post.html" %url_post% --cookie %cookie%
+    curl -# -fo "%download_dir%%name%_list.html" %url_list% --cookie %cookie% --user-agent %useragent% --connect-timeout %timeouts%
+	curl -# -fo "%download_dir%%name%_post.html" %url_post% --cookie %cookie% --user-agent %useragent% --connect-timeout %timeouts%
 
 EXIT /B 0
